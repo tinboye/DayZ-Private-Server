05acd3d2585c44aebde29ab586b2c6b3fdf416ea
Frank Harris <iaretechnician@gmail.com>
2016-03-26 14:31:17 -0400

    Added @dayz_overpoch_server

--- @START_OVERPOCH.bat
+++ @START_OVERPOCH.bat
@@ -8,7 +8,7 @@ echo.
 echo.
 echo.
 echo Starting server...
-start .\arma2oaserver.exe -mod=@dayzoverwatch;@dayz_epoch;@dayz_epoch_server -name=infected -config=config\overpoch\server.cfg -cfg=config\overpoch\basic.cfg -profiles=config\overpoch
+start .\arma2oaserver.exe -mod=@dayzoverwatch;@dayz_epoch;@dayz_overpoch_server -name=infected -config=config\overpoch\server.cfg -cfg=config\overpoch\basic.cfg -profiles=config\overpoch
 .\MySQL\cecho {0A}	OK.{07} 
 echo.
 echo.
